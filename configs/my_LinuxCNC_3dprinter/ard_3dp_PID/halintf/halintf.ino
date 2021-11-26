1034659371751927e95413071729f6dfce6ce976
Travis Farmer <travis.farmer@outlook.com>
2021-11-26 15:15:45 -0500

    comit to save changes after potential corruption

--- configs/my_LinuxCNC_3dprinter/ard_3dp_PID/halintf/halintf.ino
+++ configs/my_LinuxCNC_3dprinter/ard_3dp_PID/halintf/halintf.ino
@@ -1,32 +1,15 @@
-#include "ArduPID.h"
-ArduPID myControllerA;
-ArduPID myControllerB;
-double inputA;
-double outputA;
-double inputB;
-double outputB;
-double p = 15;
-double i = .3;
-double d = 0;
-double set_a = 0.0;
-double set_b = 0.0;
+void SendPacket(byte Addr, int Data) {
+  Serial.write((Addr & 0xf0) >> 4);
+  Serial.write(0x80 | (Addr & 0x0f));
+  Serial.write(0x40 | (Data & 0xf000) >> 12);
+  Serial.write(0xc0 | (Data & 0xf00) >> 8);
+  Serial.write(0x20 | (Data & 0xf0) >> 4);
+  Serial.write(0xa0 | (Date & 0xf));
+}
 
 void setup() {
     Serial.begin(9600);
-    myControllerA.begin(&inputA, &outputA, &set_a, p, i, d);
-    myControllerA.setOutputLimits(0, 255);
-    myControllerA.setBias(255.0 / 2.0);
-    myControllerA.setWindUpLimits(-10, 10); // Groth bounds for the integral term to prevent integral wind-up
-    
-    myControllerA.start();
-    
-    myControllerB.begin(&inputB, &outputB, &set_b, p, i, d);
-    myControllerB.setOutputLimits(0, 255);
-    myControllerB.setBias(255.0 / 2.0);
-    myControllerB.setWindUpLimits(-10, 10); // Groth bounds for the integral term to prevent integral wind-up
-    
-    myControllerB.start();
-}   
+}
 
 uint8_t adc=0;
 uint8_t firstinByte=0;
@@ -56,42 +39,11 @@ void loop() {
         }
         firstinByte = inByte;
     }
-    int tempC = (double)resistanceToC(inputToResistance(analogRead(adc)));
-    if (adc == 0) {
-      inputA = tempC;
-       myControllerA.compute();
-      analogWrite(7,outputA);
-    } else if (adc == 1) {
-      inputB = tempC;
-       myControllerB.compute();
-      analogWrite(6,outputB);
-    }
-    
-    uint16_t v = tempC | (adc << 11);
+
+    uint16_t v = analogRead(adc) | (adc << 11);
     //if(digitalRead(pinmap[adc])) v |= (1<<10);
     Serial.write((v >> 7) | 0x80);
     Serial.write(v & 0x7f);
     adc = (adc + 1) % 6;
 }
-float inputToResistance(float input) {
-  // funtion to convert the input value to resistance
-  // the value of the 'other' resistor
-  float SERIESRESISTOR = 10000;
-  return SERIESRESISTOR / (1024 / input - 1);
-}
-int resistanceToC(float resistance) {
-  // funtion to convert resistance to c
-  // temp/resistance for nominal
-  float THERMISTORNOMINAL = 118000;
-  float TEMPERATURENOMINAL = 25;
-  // beta coefficent
-  float BCOEFFICIENT = 3950;
-  float steinhart;
-  steinhart = resistance / THERMISTORNOMINAL;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart -= 273.15;   // convert to C
-  return (int)steinhart;
-}
+
