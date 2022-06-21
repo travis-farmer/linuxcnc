34e22304a8b894f38181d46e5e69afab75cce602
Travis Farmer <travis.farmer@outlook.com>
2022-06-21 10:52:32 -0400

    adding temp deps install script

---
+++ probe_basic_deps.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+apt install python-pyqt5 python-pyqt5.qtquick python-dbus.mainloop.pyqt5 python-pyqt5.qtopengl python-pyqt5.qsci python-pyqt5.qtmultimedia qml-module-qtquick-controls gstreamer1.0-plugins-bad libqt5multimedia5-plugins pyqt5-dev-tools python-dev python-wheel python-setuptools python-pip git python-pyqtgraph python-pyqt5.qtwebkit
