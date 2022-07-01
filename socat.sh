7a853bf34225a6b5b98edfff5ff4f84c1ce27b0f
Travis Farmer <travis.farmer@outlook.com>
2022-07-01 14:45:21 -0400

    added notes

--- socat.sh
+++ socat.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 socat /dev/ttyS2,b115200,raw,echo=0 TCP:192.168.123.30:7000
+
+[Unit]
+
+Description=SocatScript
+
+After=default.target
+
+[Service]
+
+ExecStart=/PATH/TO/socat.sh
+ExecStop=killall socat
+
+[Install]
+
+WantedBy=default.target
