7bbface7b2bbf212e1a9026b3b10b4960687cd5c
travis-farmer <travis.farmer@outlook.com>
2024-10-19 01:56:45 -0400

    add toolrpm.py comp

---
+++ toolrpm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import re
+import hal
+import linuxcnc
+
+h = hal.component("toolrpm")
+
+h.newpin("max-rpm", hal.HAL_FLOAT, hal.HAL_OUT)
+h.ready()
+
+# create a connection to the status channel
+s = linuxcnc.stat()
+n = 0
+
+try:
+    while 1:
+        s.poll() # get values from the status channel
+        current_tool = s.tool_in_spindle # get curent tool-number
+        if s.tool_in_spindle != 0: # a tool is loaded
+            tool_comment = s.toolinfo(current_tool)['comment'] # get the comment string of the loaded tool
+            m = re.search(r'(?<=r=)\d+', tool_comment) # search the comment string for a one- or two-digit number after 'r='
+            if m is not None: # comment string contains the information
+                n = float(m.group(0))
+            else: # comment string does not contain the information
+                n = 24
+        else:
+            n = 24
+        # set hal pin
+        h['max-rpm'] = n
+
+
+except KeyboardInterrupt:
+    raise SystemExit
+
+
