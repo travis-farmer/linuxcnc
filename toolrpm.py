82671e7262ea18a46af325f19824de63f7fae19f
travis-farmer <travis.farmer@outlook.com>
2024-10-20 15:49:17 -0400

    make less blocking

--- toolrpm.py
+++ toolrpm.py
@@ -2,6 +2,34 @@
 
 import hal
 import linuxcnc
+import signal
+import time
+
+class PeriodicTimer:
+    def __init__(self, interval, handler):
+        self.interval = interval
+        self.handler = handler
+        self.signal = signal.SIGALRM
+
+    def start(self):
+        signal.signal(self.signal, self.handle_timeout)
+        signal.setitimer(signal.ITIMER_REAL, self.interval, self.interval)
+
+    def handle_timeout(self, signum, frame):
+        self.handler()
+
+def get_val():
+    s.poll() # get values from the status channel
+    current_tool = s.tool_in_spindle # get curent tool-number
+    inifile = linuxcnc.ini(s.ini_filename)
+
+    if s.tool_in_spindle != 0: # a tool is loaded
+        tool_max = inifile.find("TOOL_MAX", current_tool) or "24000.0"
+        n = float(tool_max)
+    else:
+        n = 24000.0
+    # set hal pin
+    h['max-rpm'] = n
 
 h = hal.component("toolrpm")
 
@@ -14,20 +42,12 @@ c = linuxcnc.command()
 
 n = 0
 
+timer = PeriodicTimer(0.001, get_val)  # 2 seconds interval
+timer.start()
+
 try:
-    while 1:
-        s.poll() # get values from the status channel
-        current_tool = s.tool_in_spindle # get curent tool-number
-        inifile = linuxcnc.ini(s.ini_filename)
-
-        if s.tool_in_spindle != 0: # a tool is loaded
-            tool_max = inifile.find("TOOL_MAX", current_tool) or "24000.0"
-            n = float(tool_max)
-        else:
-            n = 24000.0
-        # set hal pin
-        h['max-rpm'] = n
-
+    while True:
+        time.sleep(1)  # Do other work
 
 except KeyboardInterrupt:
     raise SystemExit
